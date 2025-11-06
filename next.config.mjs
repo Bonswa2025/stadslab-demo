5d361feb2d956e54d034b5e10a389b79df775477
Bonswa2025 <weshley@gmail.com>
2025-11-06 19:07:45 +0100

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: { esmExternals: 'loose' },
+  webpack: (config) => {
+    config.resolve = config.resolve || {};
+    config.resolve.alias = { ...(config.resolve.alias || {}), 'pdfjs-dist/build/pdf.worker.min.mjs': 'pdfjs-dist/build/pdf.worker.min.js' };
+    return config;
+  },
   output: 'standalone'
 };
 export default nextConfig;

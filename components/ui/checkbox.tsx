8cbad367ed37d7d6e806f72646f105bc012b77f6
Bonswa2025 <weshley@gmail.com>
2025-11-06 20:09:14 +0100

    Refactor Checkbox component to support checked state

--- components/ui/checkbox.tsx
+++ components/ui/checkbox.tsx
@@ -1,7 +1,37 @@
 'use client';
 import * as React from 'react';
-export interface CheckboxProps extends React.InputHTMLAttributes<HTMLInputElement> {}
-export function Checkbox({ className='', ...props }: CheckboxProps){
-  return <input type="checkbox" className={`h-4 w-4 rounded border border-slate-300 ${className}`} {...props} />;
+
+export interface CheckboxProps
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange' | 'checked'> {
+  checked?: boolean;
+  onCheckedChange?: (checked: boolean) => void;
+  className?: string;
+  [key: string]: any;
 }
+
+export function Checkbox({
+  className = '',
+  checked,
+  onCheckedChange,
+  disabled,
+  ...props
+}: CheckboxProps) {
+  return (
+    <label
+      className={`inline-flex items-center gap-2 cursor-pointer ${
+        disabled ? 'opacity-60 cursor-not-allowed' : ''
+      } ${className}`}
+    >
+      <input
+        type="checkbox"
+        checked={!!checked}
+        onChange={(e) => onCheckedChange?.(e.currentTarget.checked)}
+        disabled={disabled}
+        {...props}
+      />
+      <span className="text-sm text-slate-700"></span>
+    </label>
+  );
+}
+
 export default Checkbox;

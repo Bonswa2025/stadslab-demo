1711917db255c74eee193d1ed87a85acb4619060
Bonswa2025 <weshley@gmail.com>
2025-11-08 21:11:28 +0100

    Delete app directory

--- app/manuals/page.jsx
+++
@@ -1,4 +0,0 @@
-'use client';
-import dynamic from 'next/dynamic';
-const FrontofficeManuals = dynamic(() => import('@/components/manuals/FrontofficeManuals'), { ssr: false });
-export default function Page(){ return <FrontofficeManuals />; }

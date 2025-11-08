491d48d3d4e2d4bc7e1f095b02d88640db9a981e
Bonswa2025 <weshley@gmail.com>
2025-11-08 21:11:13 +0100

    Delete app/backoffice directory

--- app/backoffice/page.tsx
+++
@@ -1,4 +0,0 @@
-'use client';
-import dynamic from 'next/dynamic';
-const StadslabBackoffice = dynamic(() => import('@/components/stadslab/StadslabBackoffice'), { ssr: false });
-export default function Page(){ return <StadslabBackoffice />; }
